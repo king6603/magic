071008e9cb0fd38574b830e3539dfa73a0038b71
xujiangtao <xujiangtao@chuchujie.com>
2017-11-30 22:42:10 +0800

    Initial commit

---
+++ src/main/java/com/jt/magic/MagicApplication.java
@@ -0,0 +1,12 @@
+package com.jt.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicApplication.class, args);
+	}
+}
